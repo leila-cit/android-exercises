4944064f9d930d090aaa4e7e3de9f93241ff2e92
Leila Gomes Ferreira <leila@lnxcit012261cps.cit>
2018-08-24 14:13:24 -0300

    Improving to Butterknife

--- app/src/main/java/com/example/leila/exercise1/MainActivity.java
+++ app/src/main/java/com/example/leila/exercise1/MainActivity.java
@@ -14,29 +14,28 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnList, btnClose;
+    @BindView(R.id.btnList)
+    Button btnList;
+
+    @BindView(R.id.btnClose)
+    Button btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnList = (Button)findViewById(R.id.btnList);
-        btnList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Botão 1 clicado", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        btnClose = (Button)findViewById(R.id.btnClose);
-        btnClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.this.finish();
-                System.exit(0);
-            }
-        });
+        ButterKnife.bind(this);
     }
 
+    @OnClick(R.id.btnList)
+    public void OnBtnListClicked(View view){
+        Toast.makeText(MainActivity.this, "Botão 1 clicado", Toast.LENGTH_SHORT).show();
+    }
+
+    @OnClick(R.id.btnClose)
+    public void OnBtnCloseClicked(View view){
+        MainActivity.this.finish();
+        System.exit(0);
+    }
 
 }
