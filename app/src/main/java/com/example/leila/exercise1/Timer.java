e76ff3c4ebc5d95a83b353dd7d7a6847f985b033
Leila Gomes Ferreira <leila@lnxcit012261cps.cit>
2018-08-27 11:41:42 -0300

    Exercise 2

---
+++ app/src/main/java/com/example/leila/exercise1/Timer.java
@@ -0,0 +1,30 @@
+package com.example.leila.exercise1;
+
+import android.os.CountDownTimer;
+
+public class Timer extends CountDownTimer {
+
+    private CountdownListener listener;
+    /**
+     * @param millisInFuture    The number of millis in the future from the call
+     *                          to {@link #start()} until the countdown is done and {@link #onFinish()}
+     *                          is called.
+     * @param countDownInterval The interval along the way to receive
+     *                          {@link #onTick(long)} callbacks.
+     */
+    public Timer(long millisInFuture, long countDownInterval, CountdownListener listener) {
+        super(millisInFuture, countDownInterval);
+
+        this.listener = listener;
+    }
+
+    @Override
+    public void onTick(long millisUntilFinished) {
+
+    }
+
+    @Override
+    public void onFinish() {
+        listener.onFinish();
+    }
+}
