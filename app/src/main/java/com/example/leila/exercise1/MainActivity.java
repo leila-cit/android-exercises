2518a4dbd8b364dc37ee43d36edfe7a189ed510a
Leila Gomes Ferreira <leila@ciandt.com>
2018-09-05 08:43:26 -0300

    Exercise 3 - flavors for pokemon teams

--- app/src/main/java/com/example/leila/exercise1/MainActivity.java
+++ app/src/main/java/com/example/leila/exercise1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.leila.exercise1;
 
 import android.content.Intent;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
