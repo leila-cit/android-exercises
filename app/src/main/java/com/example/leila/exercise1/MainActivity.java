d54b832f5a29ff3e09a2c0079c16fb1a69b8b865
Leila Gomes Ferreira <leila@ciandt.com>
2018-09-06 09:15:10 -0300

    Exercise 3 - improvements

--- app/src/main/java/com/example/leila/exercise1/MainActivity.java
+++ app/src/main/java/com/example/leila/exercise1/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.leila.exercise1;
 
 import android.content.Intent;
-import android.app.ActionBar;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
